8aa56abed56004007ae5dbd773e92960f859e245
Ricky Ayoub <vgmoose@gmail.com>
2014-06-02 02:14:50 -0400

    a

--- js/lightbox_library.js
+++ js/lightbox_library.js
@@ -141,7 +141,8 @@ function logEmIn()
            $.cookie("key", pass, {expires: date.getTime() + 30000000000});
            if (document.referrer.indexOf("/deku-scrub/blag/blog/") > 0)
                 location.href = document.referrer;
-           location.href = '/deku-scrub/blag/';
+           else
+                location.href = '/deku-scrub/blag/';
            bool = false;
            }
            });
